f226c1b08cf5224fe420850c055735a4b560e5e5
Ahmet Safa Kara <ahmet_safa_kara@epam.com>
2022-07-29 11:21:17 +0200

    first commit

---
+++ src/test/java/com/epam/askara/MainTest.java
@@ -0,0 +1,13 @@
+package com.epam.askara;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void test(){
+        assertEquals(1,1);
+    }
+}
